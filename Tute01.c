7ab77ae0ac317f7b900eb395b7e547ba653c6e39
IT21021534 <87350551+Priyantha-IT21021534@users.noreply.github.com>
2021-07-13 03:43:45 +0000

    tute1 completed

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,17 @@
 #include <stdio.h>
 
 int main() {
-  
+  int mark1, mark2;
+
+  //get marks
+  printf("Insert mark 1: ");
+  scanf("%d", &mark1);
+  printf("Insert mark 2: ");
+  scanf("%d", &mark2);
+
+  //output results
+  printf("Average of two given marks: %.2f", (mark1 + mark2) / 2.0);
+
   return 0;
 }
 
